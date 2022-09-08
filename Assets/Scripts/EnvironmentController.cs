6f482ce9c551e5e1a6f37897a3a44f40b4ccf93f
CubeMD <markelovdp@gmail.com>
2022-09-08 17:33:46 -0400

    Added new controls, changed sensor, added trained model

---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MLAgentsDebugTool.Duplicator
+{
+    public class EnvironmentController : MonoBehaviour
+    {
+        [SerializeField] private GameObject ground;
+        [HideInInspector] public float halfGroundSize;
+        
+        [SerializeField] private Target targetPrefab;
+        [SerializeField] private int numTargets;
+    
+        public readonly List<Target> targets = new List<Target>();
+        
+
+        public void Initialize()
+        {
+            halfGroundSize = ground.transform.localScale.x / 2;
+            
+            for (int i = 0; i < numTargets; i++)
+            {
+                Target t = Instantiate(targetPrefab, transform);
+                t.Initialize(halfGroundSize);
+                targets.Add(t);
+            }
+        }
+
+        public void ResetTargets()
+        {
+            foreach (Target target in targets)
+            {
+                target.Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            ResetTargets();
+        }
+    }
+}
